9fe4833bdf7592ca50aaf33bc08bcb6c044b3259
sindhyakrs <sindhyakrs@gmail.com>
2020-07-17 17:39:28 +1000

    added restassured sample to framework

--- src/test/java/autoapp/automation/stepDef/RegisterStepDef.java
+++ src/test/java/autoapp/automation/stepDef/RegisterStepDef.java
@@ -8,6 +8,9 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.openqa.selenium.WebDriver;
 
 public class RegisterStepDef extends BaseUtil {
@@ -15,6 +18,8 @@ public class RegisterStepDef extends BaseUtil {
     private BaseUtil baseUtil;
     private PersonalInfoPage personalInfoPage;
     private RegisterPage registerPage;
+    private RequestSpecification request = RestAssured.given();
+    private Response response;
 
     public RegisterStepDef(BaseUtil baseUtil) {
         this.baseUtil = baseUtil;
@@ -25,12 +30,18 @@ public class RegisterStepDef extends BaseUtil {
     @Given("^Automation practice application is opened$")
     public void automation_practice_application_is_opened() throws Throwable {
         registerPage.openApplicaiton();
+        response =
+                request.baseUri("https://gorest.co.in/")
+                        .basePath("public-api/users")
+                        .log().all()
+                        .when()
+                        .get();
     }
 
     @When("^I click sign in$")
     public void i_click_sign_in() throws Throwable {
         registerPage.clickSignIn();
-
+        response.then().log().all();
     }
 
     @And("^I create account with emailid \"([^\"]*)\"$")
